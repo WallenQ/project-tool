f9e81b1eb3a26bfe3d3ad285ddfd633919bdc558
WallenQ <872465234@qq.com>
2021-07-20 10:14:40 +0800

    rabbitmq发送方法-代码优化

---
+++ src/main/java/com/wallen/tool/rabbitmq/RabbitMQSend.java
@@ -0,0 +1,72 @@
+package com.wallen.tool.rabbitmq;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.apache.commons.lang.SerializationUtils;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * rabbitmq发送方法
+ *
+ * @author wallen
+ * @date 20210719
+ */
+public class RabbitMQSend {
+    /**
+     * 定义一个队列
+     */
+    private final static String QUEUE_NAME = "QUEUE_NAME";
+
+    public static void main(String[] args) {
+        //申明通道发送消息的队列，把消息发送至消息队列‘hello’
+        Map<String, Object> map = new HashMap<>(5);
+        map.put("batteryVoltage", 430);
+        map.put("deviceId", 6142);
+        map.put("deviceType", 3);
+        map.put("batterManufacturerNumber", 2);
+        map.put("maxChargingCurrent", 280);
+        try {
+            send(QUEUE_NAME,SerializationUtils.serialize((Serializable) map));
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 发送方法
+     *
+     * @param queue
+     * @param sendData
+     * @throws IOException
+     * @throws TimeoutException
+     */
+    public static void send (String queue,byte[] sendData) throws IOException, TimeoutException {
+        //创建一个连接
+        ConnectionFactory factory = new ConnectionFactory();
+        //连接本地，如果需要指定到服务，需在这里指定IP
+        factory.setHost("192.168.0.11");
+        factory.setPort(5672);
+        factory.setUsername("user1");
+        factory.setPassword("123456");
+        Connection connection = factory.newConnection();
+        String exchange = "section6";
+
+        //Declaring a queue is idempotent - 如果队列不存在则会创建一个队列
+        //消息内容为byte array, so可以自己随意编码
+        //创建一个通道
+        Channel channel = connection.createChannel();
+        Map<String ,Object> map = new HashMap<>(1);
+        channel.queueDeclare(queue, true, false, false, map);
+        channel.basicPublish(exchange, queue, null, sendData);
+        //消息发送完成后，关闭通道和连接
+        channel.close();
+
+        connection.close();
+    }
+}
